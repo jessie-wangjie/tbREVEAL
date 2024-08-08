35cb5eed2e049e688ae7e70d9f571a0e75266c95
jessie <jwang@tome.bio>
2024-08-07 21:04:04 -0400

    fix reference issue

--- bin/get_project_info.py
+++ bin/get_project_info.py
@@ -19,7 +19,7 @@ def get_project_info(project_id):
     metadata_query_result = cur.fetchall()
     sample_info = {}
     for result in metadata_query_result:
-        sample_name, sequencing_sample_name,attb, attp, primers, hybrid_capture_panel, probes_or_barcodes, umi_type, species, probes, donor, group = result
+        sample_name, sequencing_sample_name,attb, attp, primers, hybrid_capture_panel, umi_type, species, probes, donor, group = result
 
         R1, R2 = None, None  # Initialize R1 and R2 for each sample_name
         for entry in os.listdir('.'):
